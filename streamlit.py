40bb8a0704a717dc20a2db5faaff78125c073907
jeffong2000 <73022595+jeffong2000@users.noreply.github.com>
2022-07-19 12:35:20 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -432,3 +432,4 @@ if uploaded_file is not None:
         st.set_option('deprecation.showPyplotGlobalUse', False)
         st.pyplot()
     st.success("Analysis successful")
+    st.dataframe(pred) 
