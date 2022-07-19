ad3c5cd0b0349d4dc9d15bf1848fabdafd4d80bf
jeffong2000 <73022595+jeffong2000@users.noreply.github.com>
2022-07-19 15:41:53 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -404,7 +404,7 @@ if uploaded_file is not None:
         plt.legend()
         st.set_option('deprecation.showPyplotGlobalUse', False)
         st.pyplot()
-        st.bar_chart(pred['Delivery quality'])
+        st.bar_chart(pred['Delivery quality'].value_counts())
     st.success("Analysis successful")
     st.dataframe(pred)
 
